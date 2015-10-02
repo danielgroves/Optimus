ba5cddc530e58752445bec15ac3e4f82167b306e
Daniel Groves <hello@danielgroves.net>
2015-10-03 00:56:14 +0100

    Load image into RMagick
    This is the framework around which image manipulation will take place

---
+++ app/image.rb
@@ -0,0 +1,12 @@
+require 'rmagick'
+
+# Image Model
+class Image
+  def initialize(image_data)
+    @image = Magick::Image::from_blob image_data
+  end
+
+  def finish
+    @image[0].to_blob
+  end
+end
--- app/optimus.rb
+++ app/optimus.rb
@@ -1,4 +1,5 @@
 require_relative 'resource_request'
+require_relative 'image'
 
 # Rack application for on-the-fly image optimisation.
 class Optimus
@@ -6,9 +7,10 @@ class Optimus
     request = Rack::Request.new env
 
     resource = ResourceRequest.new.get request.path_info
+    image = Image.new resource.body
 
     response = Rack::Response.new
-    response.write resource.body
+    response.write image.finish
     response['Content-Type'] = 'image/jpeg'
     response.status = resource.code
 
